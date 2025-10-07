4d984db77c7103d7699478e7fa94278a7aad1f76
hugo <hugotronchewu@gmail.com>
2025-10-07 09:31:21 +0200

    z

--- main.go
+++ main.go
@@ -1,33 +1,40 @@
 package main
 
 import (
-	"net/http"
-	"text/template"
+    "net/http"
+    "text/template"
 )
 
 type Game struct {
-	Board         [6][7]int // 0=vide, 1=rouge, 2=jaune
-	CurrentPlayer int
+    Board         [6][7]int // 0 = vide, 1 = rouge, 2 = jaune
+    CurrentPlayer int
 }
 
 type GamePageData struct {
-	PageTitle     string
-	Board         [6][7]int
-	CurrentPlayer int
-	Winner        int
+    PageTitle     string
+    Board         [6][7]int
+    CurrentPlayer int
+    Winner        int
+}
+
+
+func CheckWinner(board [6][7]int) int {
+    return 0 // 0 = pas de gagnant
 }
 
 func main() {
-	tmpl := template.Must(template.ParseFiles("layout.html"))
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		Board := 1
-		data := GamePageData{
-			PageTitle:     "Puissance 4",
-			Board:         Game.Board,
-			CurrentPlayer: Game.CurrentPlayer,
-			Winner:        CheckWinner(),
-		}
-		tmpl.Execute(w, data)
-	})
-	http.ListenAndServe(":8080", nil)
+    game := &Game{}
+    tmpl := template.Must(template.ParseFiles("layout.html"))
+
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        data := GamePageData{
+            PageTitle:     "Puissance 4",
+            Board:         game.Board,
+            CurrentPlayer: game.CurrentPlayer,
+            Winner:        CheckWinner(game.Board),
+        }
+        tmpl.Execute(w, data)
+    })
+
+    http.ListenAndServe(":8080", nil)
 }
