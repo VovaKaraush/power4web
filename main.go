277f28aafd8333b2be2e962ad0cefeffba13f834
Vova <starper200765@gmail.com>
2025-10-07 11:00:17 +0200

    nothing works

--- main.go
+++ main.go
@@ -2,31 +2,14 @@ package main
 
 import (
 	"net/http"
+	"power4web/modules/modules"
 	"text/template"
 )
 
-type Game struct {
-	Board         [6][7]int // 0=vide, 1=rouge, 2=jaune
-	CurrentPlayer int
-}
-
-type GamePageData struct {
-	PageTitle     string
-	Board         [6][7]int
-	CurrentPlayer int
-	Winner        int
-}
-
 func main() {
 	tmpl := template.Must(template.ParseFiles("layout.html"))
+	board := modules.NewBoard()
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		Board := 1
-		data := GamePageData{
-			PageTitle:     "Puissance 4",
-			Board:         Game.Board,
-			CurrentPlayer: Game.CurrentPlayer,
-			Winner:        CheckWinner(),
-		}
 		tmpl.Execute(w, data)
 	})
 	http.ListenAndServe(":8080", nil)
