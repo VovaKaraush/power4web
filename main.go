a250a06a1305fa4de25021a117621ea1812553df
Vova Karaush <starper200765@gmail.com>
2025-10-28 10:51:31 +0100

    nouvel lancement, check win teoriquement bon put piece teoriquement bon

--- main.go
+++ main.go
@@ -1,40 +1,67 @@
 package main
 
-import (
-    "net/http"
-    "text/template"
-)
+import "fmt"
 
 type Game struct {
-    Board         [6][7]int // 0 = vide, 1 = rouge, 2 = jaune
-    CurrentPlayer int
+	Board  [][]int // Plateau 6x7 || Horizontales X Verticales || lignes X colonnes
+	Player int     // Joueur actuel (1 ou 2)
+	Winner int     // 0 = pas de gagnant, 1 ou 2 = gagnant
 }
 
-type GamePageData struct {
-    PageTitle     string
-    Board         [6][7]int
-    CurrentPlayer int
-    Winner        int
+func NewGame() *Game { // le tableau est formé en descendant sa generation ressemble a une fleche en diagonale-
+	board := make([][]int, 6)
+	for i := range board {
+		board[i] = make([]int, 7)
+	}
+	return &Game{Board: board, Player: 1, Winner: 0}
 }
 
+func (g *Game) PutPiece(col int, player int) bool { // va renvoyer un bool, vrai pour signaler la reussite de l'operation,
+	for i := 5; i > 0; i-- { // si renvoit false ca veut dire que la collonne choisie etait pleine
+		if g.Board[i][col] == 0 {
+			return true
+		}
+	}
+	return false
+}
 
-func CheckWinner(board [6][7]int) int {
-    return 0 // 0 = pas de gagnant
+func (g *Game) checkWin() int { //horizontal then vertical then diagonal+ and then diagonal-
+	for col := 0; col < 6; col++ { // verticales
+		for lgn := 5; lgn >= 0; lgn-- { // horizontales
+			if g.Board[lgn][col] != 0 {
+				checked := g.Board[lgn][col]
+				if g.Board[lgn][col] == g.Board[lgn][col+1] && g.Board[lgn][col+1] == g.Board[lgn][col+2] && g.Board[lgn][col+2] == g.Board[lgn][col+3] { // check de win horizontal
+					return (checked)
+				} else if g.Board[lgn][col] == g.Board[lgn+1][col] && g.Board[lgn+1][col] == g.Board[lgn+2][col] && g.Board[lgn+2][col] == g.Board[lgn+3][col] { // check horizontal
+					return (checked)
+				} else if g.Board[lgn][col] == g.Board[lgn+1][col+1] && g.Board[lgn+1][col+1] == g.Board[lgn+2][col] && g.Board[lgn+2][col+2] == g.Board[lgn+3][col+3] { // check diagonal +
+					return (checked)
+				} else if g.Board[lgn][col] == g.Board[lgn-1][col+1] && g.Board[lgn-1][col+1] == g.Board[lgn-2][col] && g.Board[lgn-2][col+2] == g.Board[lgn-3][col+3] { // check diagonal -
+					return (checked)
+				}
+			}
+		}
+		return 0
+	}
+}
+
+func InputScan() int {
+	var x int
+	_, err := fmt.Scanln(&x)
+	if err != nil {
+		fmt.Println("Entrée invalide, réessaie.")
+		AssignValue()
+	}
+	return x
 }
 
 func main() {
-    game := &Game{}
-    tmpl := template.Must(template.ParseFiles("layout.html"))
-
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        data := GamePageData{
-            PageTitle:     "Puissance 4",
-            Board:         game.Board,
-            CurrentPlayer: game.CurrentPlayer,
-            Winner:        CheckWinner(game.Board),
-        }
-        tmpl.Execute(w, data)
-    })
-
-    http.ListenAndServe(":8080", nil)
+	var game *Game = NewGame()
+	for game.Winner == 0 {
+		fmt.Print("Joueur ")
+		fmt.Print(game.Player)
+		fmt.Print(" choisis la colonne dans laquelle tu veux mettre la piece")
+		x := InputScan()
+		game.PutPiece(x, game.Player)
+	}
 }
